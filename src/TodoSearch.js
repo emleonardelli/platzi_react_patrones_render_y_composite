ac9b04e86f944f319b528cbe71882603a4acb528
Ezequiel Mella Leonardelli <emleonardelli@gmail.com>
2021-12-03 09:09:07 -0300

    Simple events

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,8 +2,16 @@ import React from 'react';
 import './TodoSearch.css';
 
 function TodoSearch() {
+  const onSearchValueChange = (ev) => {
+    console.log(ev.target.value);
+  }
+
   return (
-    <input className="TodoSearch" placeholder="Cebolla" />
+    <input
+      onChange={onSearchValueChange}
+      className="TodoSearch"
+      placeholder="Cebolla"
+    />
   );
 }
 
