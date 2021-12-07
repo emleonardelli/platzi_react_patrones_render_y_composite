7972921bbfe2edc9bfb0387442751b548d3de857
Ezequiel Mella Leonardelli <emleonardelli@gmail.com>
2021-12-07 09:16:28 -0300

    Local Storage en un custom hook

--- src/App/index.js
+++ src/App/index.js
@@ -1,27 +1,33 @@
 import React, { useState } from 'react';
 import { AppUI } from './AppUI';
 
-const defaultTodos = [
-  { text: 'Cortar cebolla', completed: true },
-  { text: 'Tomar el cursso de intro a React', completed: false },
-  { text: 'Llorar con la llorona', completed: true },
-  { text: 'LALALALAA', completed: false },
-];
+//const defaultTodos = [
+//  { text: 'Cortar cebolla', completed: true },
+//  { text: 'Tomar el cursso de intro a React', completed: false },
+//  { text: 'Llorar con la llorona', completed: true },
+//  { text: 'LALALALAA', completed: false },
+//];
+
+function useLocalStorage(itemName, initialValue) {
+  const localStorageItem = localStorage.getItem(itemName);
+  let parsedItem;
+  if (!localStorageItem) {
+    localStorage.setItem(itemName, JSON.stringify(initialValue));
+    parsedItem = initialValue;
+  } else {
+    parsedItem = JSON.parse(localStorageItem);
+  }
+  const [item, setItem] = useState(parsedItem);
+  const saveItem = (newItem) => {
+    localStorage.setItem(itemName, JSON.stringify(newItem));
+    setItem(newItem);
+  }
+  return [item, saveItem];
+}
 
 function App() {
-  const localStorageTodos = localStorage.getItem('TODOS_V1');
-  let parsedTodos;
-
-  if (!localStorageTodos) {
-    localStorage.setItem('TODOS_V1', JSON.stringify(defaultTodos));
-    parsedTodos = [];
-  } else {
-    parsedTodos = JSON.parse(localStorageTodos);
-  }
-
-  const [todos, setTodos] = useState(parsedTodos);
+  const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
   const [searchValue, setSearchValue] = useState('');
-
   const completedTodos = todos.filter(todo => !!todo.completed).length;
   const totalTodos = todos.length;
 
@@ -32,11 +38,6 @@ function App() {
     searchedTodos = todos.filter(todo => {
       return todo.text.toLowerCase().includes(searchValue.toLowerCase());
     })
-  }
-
-  const saveTodos = (newTodos) => {
-    localStorage.setItem('TODOS_V1', JSON.stringify(newTodos));
-    setTodos(newTodos);
   }
 
   const completeTodo = (text) => {
